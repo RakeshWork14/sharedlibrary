3feb2236fe4d2d7ad679089dc40fbf063cbc2f3b
Rakesh <rakeshwork14.email.com>
2025-06-17 13:13:37 +0530

    added shared library

---
+++ src/com/i27academy/builds/Docker_methods.groovy
@@ -0,0 +1,20 @@
+// we need to place all the methods in the src folder only
+package.com.i27academy.builds
+
+// all the methods
+class Docker{
+    def jenkins
+    Docker(jenkins){
+        this.jenkins = jenkins
+    }
+
+    // Build app Method
+    def buildApp(appName){
+        jenkins.sh """
+            echo "Building the $appName Application"
+            mvn clean package -DskipTests=true
+        """
+    }
+}
+
+
